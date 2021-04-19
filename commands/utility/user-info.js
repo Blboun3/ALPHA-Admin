6851886866e9b6e1cd97939b8f94066eb5f72e51
Blboun3 <cyrda518@gmail.com>
2021-04-19 09:54:46 +0200

    blbosti

--- commands/utility/user-info.js
+++ commands/utility/user-info.js
@@ -4,6 +4,11 @@ module.exports = {
 	cooldown: 10,
 	description: 'Vypíše informace o daném uživateli serveru',
 	execute(message) {
-		message.channel.send(`**Jméno:** ${message.author.username}\n**ID uživatele:** ${message.author.id}`);
+		if (!message.mentions.users.size){
+			return message.channel.send(`**Jméno:** ${message.author.username}\n**ID uživatele:** ${message.author.id}`);
+		}
+		const taggedUser = message.mentions.members.first();
+
+		return message.channel.send(`**Jméno:** ${taggedUser.tag}\n**ID uživatele:** ${taggedUser.id}`);
 	},
 };
