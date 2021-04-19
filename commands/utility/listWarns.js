cf788c1bdf62b3ffeaf5fc08bc9763092b450863
Blboun3 <cyrda518@gmail.com>
2021-04-19 20:31:23 +0200

    WIP

---
+++ commands/utility/listWarns.js
@@ -0,0 +1,35 @@
+module.exports = {
+	name: 'list-warns',
+	aliases: ["lwarn", "warns", "warnings"],
+	usage: "[user]",
+	execute(message, args, DB) {
+		if(!message.mentions.users.size){
+			DB.query(`SELECT * FROM warns WHERE userID="${message.author.id}"`, (err, result) => {
+				if(err) throw err;
+			        if(!result.length > 0){
+					return message.channel.send("Vypadá to, že jsi bezúhonný uživatel ALPHY!");
+				}
+				var out;
+				for(const element of result){
+					let output = `**${element.warnID}** | <@!${element.warnAuthor}> | ${element.reason} | ${element.warnDate} \n`;
+					out = out + output;
+				};
+				return message.channel.send(out);
+			});
+		}else{
+
+			const taggedUser = message.mentions.members.first();
+
+			DB.query(`SELECT * FROM warns WHERE userID="${taggedUser.id}"`, (err, result) => {
+				if(err) throw err;
+				var out;
+				for(const element of result){
+					let output = `**${element.warnID}** | <@!${element.warnAuthor}> | ${element.reason} | ${element.warnDate} \n`;
+					out = out + output;
+				}
+				return message.channel.send(out);
+			});
+		}
+
+	}
+}
