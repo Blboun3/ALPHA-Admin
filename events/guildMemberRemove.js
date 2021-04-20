e2b8ab5cec9477d9f55781ae963a7fce70e7041d
Blboun3 <cyrda518@gmail.com>
2021-04-20 15:20:27 +0200

    Konečně funguje odpojování a připojování, uchová role

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: 'guildMemberRemove',
+	execute(member,client,DB) {
+  		let arr = member.roles.cache.array();
+  		let rls = new Array();
+  		arr.forEach(function(rl){
+    			if(rl.id != "767628835347890196"){ // Filtr pro nepsaní role everyone
+    				rls.push(rl.id); // Zapsaní
+			}
+  		}); 		
+		DB.query(`INSERT INTO users(userID, roles) VALUES ("${member.id}", "${rls}")`, (err) => { // Uložení dat do DB
+			if(err) throw err; // Případné vyhození erroru
+		});
+	}
+};
