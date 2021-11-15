47f83974dae77081fb3c15b8166ba3fdf98bbeec
MaximMaximS <60849730+MaximMaximS@users.noreply.github.com>
2021-11-15 08:46:18 +0100

    Delete player directory

--- player/playlistAdd.js
+++
@@ -1,3 +0,0 @@
-module.exports = (client, message, queue, playlist) => {
-    message.channel.send(`${client.emotes.music} - ${playlist.title} byla přidána do playlisut (**${playlist.tracks.length}** skladeb)!`);
-};
