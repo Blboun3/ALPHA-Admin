56b1078efa1c552fb31898049a5459db05bf8903
Blboun3 <62328614+Blboun3@users.noreply.github.com>
2023-04-03 22:05:03 +0200

    FIRST RELEASE
    Yes, it's true, after some time of hard work this is official. Bot in this state will be moved to 'production' state on ALPHA server (cz) and will run (almost) 24/7 on one of my RPi 400 computers. It's not completely bug-free, but it's pretty stable.
    
    In this commit I've done a lot of bugfixing. Also I've added welcome messages with images after someone verifies.
    
    Name of this version is La Quintessenza, which is composition by Johan de Meij. Last version's name was Amélie, after the film 	Le Fabuleux Destin d'Amélie Poulain. Both have something in common - very nice music, soundtrack from Amélie is amazing, and La Quintessenza by Johan de Meij is also and amazing music.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,15 +1,82 @@
-const {Events} = require('discord.js');
-const { verified_role } = require('../public_config.json')
+const { Events, AttachmentBuilder } = require('discord.js');
+const { verified_role, welcome_channel } = require('../public_config.json')
 const application_request_processor = require('./hlp_application_request');
+const { request } = require('undici');
 const role_selector = require('./hlp_role_selector');
+const Canvas = require('@napi-rs/canvas');
+const { GlobalFonts } = require('@napi-rs/canvas');
+
+// Pass the entire Canvas object because you'll need access to its width and context
+const applyText = (canvas, text) => {
+	const context = canvas.getContext('2d');
+
+	// Declare a base size of the font
+	let fontSize = 70;
+
+	do {
+		// Assign the font to the context and decrement it so it can be measured again
+		context.font = `${fontSize -= 2}px JetBrainsMono`;
+		// Compare pixel width of the text to the canvas minus the approximate avatar size
+	} while (context.measureText(text).width > canvas.width - 320);
+
+	// Return the result to use in the actual canvas
+	return context.font;
+};
 
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
-
         // Verification
         if(interaction.customId === 'verification_btn'){
+            // Give user verified role
             interaction.member.roles.add(verified_role);
+
+            // Get welcome channel (where to send message about his appearance)
+            const chnl = interaction.guild.channels.cache.get(welcome_channel)
+            
+            // Process Image creation
+            const font = GlobalFonts.registerFromPath('./font.ttf','JetBrainsMono');
+            const canvas = Canvas.createCanvas(700, 250);
+		    const context = canvas.getContext('2d');
+
+            // Draw background image
+            const bg = await Canvas.loadImage('./welcome_background.jpg')
+            context.drawImage(bg, 0, 0, canvas.width, canvas.height);
+
+            // Draw stroke around entire image
+	        context.strokeStyle = '#0099ff'
+        	context.strokeRect(0, 0, canvas.width, canvas.height);
+
+            // Assign the decided font to the canvas
+	        context.font = applyText(canvas, interaction.member.displayName);
+	        context.fillStyle = '#ffffff';
+	        context.fillText(`${interaction.member.displayName}!`, canvas.width / 2.5, canvas.height / 1.5);
+            // Slightly smaller text placed above the member's display name
+	        context.font = '28px JetBrainsMono';
+	        context.fillStyle = '#ffffff';
+	        context.fillText('Vítej na ALPHĚ,', canvas.width / 2.5, canvas.height / 2.5);
+
+            // Process User's avatar
+            // Using undici to make HTTP requests for better performance
+	        const { body } = await request(interaction.user.displayAvatarURL({ extension: 'jpg' }));
+	        const avatar = await Canvas.loadImage(await body.arrayBuffer());
+            // Cut it to circle
+            // Pick up the pen
+	        context.beginPath();
+        	// Start the arc to form a circle
+	        context.arc(125, 125, 100, 0, Math.PI * 2, true);
+	        // Put the pen down
+        	context.closePath();
+	        // Clip off the region you drew on
+	        context.clip();
+            // Place avatar on to the background
+            context.drawImage(avatar, 25, 25, 200, 200);
+
+            const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: `welcome-user.png` });
+            console.log(`User ${interaction.user.displayName} was verified`);
+            chnl.send({content: `Vítej na ALPHĚ, <@${interaction.user.id}> - největším českém matematicko-programátorském serveru!`, files: [attachment]})
+
+            // Reply to user that verification was successfull (also so that discord doesn't have any problems XD)
             await interaction.reply({content: "Verifikace proběhla úspěšně!", ephemeral: true});
             return 
         }
